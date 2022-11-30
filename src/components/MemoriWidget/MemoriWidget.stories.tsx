8d0cd0b03fc94234c7e266f9345dded2fc1ff218
nzambello <nzambello@protonmail.com>
2022-11-30 18:29:00 +0100

    chore: cleanup

--- src/components/MemoriWidget/MemoriWidget.stories.tsx
+++ src/components/MemoriWidget/MemoriWidget.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { memori, integration, tenant, sessionID } from '../../mocks/data';
+import { memori, integration, tenant } from '../../mocks/data';
 import MemoriWidget, { Props } from './MemoriWidget';
 
 const meta: Meta = {
