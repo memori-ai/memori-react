54d1436a6195303806c2c32985f6801ae6292d67
nzambello <nzambello@protonmail.com>
2022-12-05 20:09:14 +0100

    chore: switch to tsup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
-/** @type {import('jest').Config} */
-const config = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   silent: true,
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    uuid: require.resolve('uuid'),
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@memori.ai/memori-api-client)',
+    '/node_modules/(?!microsoft-cognitiveservices-speech-sdk)',
+    '/node_modules/(?!uuid)',
+  ],
 };
-
-module.exports = config;
