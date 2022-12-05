8791dbfe69abac45ff470efeaeeaa21b10285b5f
nzambello <nzambello@protonmail.com>
2022-12-05 16:33:36 +0100

    fix: i18n import

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import {
 import memoriApiClient from '@memori.ai/memori-api-client';
 import MemoriWidget from './components/MemoriWidget/MemoriWidget';
 
-import { i18n } from './i18n';
+import i18n from './i18n';
 import { useTranslation } from 'react-i18next';
 
 export interface Props {
