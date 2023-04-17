41d8f3c0a98aafc10ea002e4af15e9b2eab2d045
nzambello <nzambello@protonmail.com>
2023-04-17 11:50:06 +0200

    test: fix date flakyness

--- src/components/DateSelector/DateSelector.test.tsx
+++ src/components/DateSelector/DateSelector.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import DateSelector from './DateSelector';
+import moment from 'moment';
 
 const dateMock = new Date(Date.UTC(2022, 8, 24, 0, 0, 0, 0));
 
@@ -18,7 +19,7 @@ it('renders DateSelector unchanged', () => {
 it('renders DateSelector with default value unchanged', () => {
   const { container } = render(
     <DateSelector
-      defaultDate={new Date(2000, 0, 1, 0, 0, 0)}
+      defaultDate={moment(`2022-08-24 00:00:00Z`)}
       onChange={jest.fn()}
     />
   );
@@ -29,7 +30,7 @@ it('renders DateSelector disabled unchanged', () => {
   const { container } = render(
     <DateSelector
       disabled
-      defaultDate={new Date(2000, 0, 1, 0, 0, 0)}
+      defaultDate={moment(`2022-08-24 00:00:00Z`)}
       onChange={jest.fn()}
     />
   );
