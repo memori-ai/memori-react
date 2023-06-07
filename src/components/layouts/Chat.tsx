eee4cf5e893dd65e7b341516a0f2dcfb65519841
nzambello <nzambello@protonmail.com>
2023-06-07 15:06:16 +0200

    refactor: chat layout with layout props

--- src/components/layouts/Chat.tsx
+++ src/components/layouts/Chat.tsx
@@ -3,12 +3,16 @@ import Spin from '../ui/Spin';
 import { LayoutProps } from '../MemoriWidget/MemoriWidget';
 
 const ChatLayout: React.FC<LayoutProps> = ({
-  header,
-  chat,
-  startPanel,
+  Header,
+  headerProps,
+  Chat,
+  chatProps,
+  StartPanel,
+  startPanelProps,
   integrationStyle,
   integrationBackground,
-  changeMode,
+  ChangeMode,
+  changeModeProps,
   sessionId,
   hasUserActivatedSpeak,
   showInstruct = false,
@@ -21,12 +25,20 @@ const ChatLayout: React.FC<LayoutProps> = ({
 
     <Spin spinning={loading} className="memori-chat-layout">
       {poweredBy}
-      {showInstruct && changeMode}
+      {showInstruct && ChangeMode && changeModeProps && (
+        <ChangeMode {...changeModeProps} />
+      )}
 
-      <div className="memori-chat-layout--header">{header}</div>
+      <div className="memori-chat-layout--header">
+        {Header && headerProps && <Header {...headerProps} />}
+      </div>
 
       <div className="memori-chat-layout--controls">
-        {sessionId && hasUserActivatedSpeak ? chat : startPanel}
+        {sessionId && hasUserActivatedSpeak && Chat && chatProps ? (
+          <Chat {...chatProps} />
+        ) : startPanelProps ? (
+          <StartPanel {...startPanelProps} />
+        ) : null}
       </div>
     </Spin>
   </>
