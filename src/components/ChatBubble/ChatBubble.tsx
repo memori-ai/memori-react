08bd938d23bc214ddd0c8c12cc22f6099dcc7b23
nzambello <nzambello@protonmail.com>
2024-11-26 14:51:22 +0200

    refactor: update marked

--- src/components/ChatBubble/ChatBubble.tsx
+++ src/components/ChatBubble/ChatBubble.tsx
@@ -53,7 +53,7 @@ marked.use({
   gfm: true,
   pedantic: true,
   renderer: {
-    link(href: string, title: string | null | undefined, text: string) {
+    link: ({ href, title, text }) => {
       const cleanHref = cleanUrl(href);
       if (cleanHref === null) {
         return text;
