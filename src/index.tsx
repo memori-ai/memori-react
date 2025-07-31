3fe2522db8a0cfdb3ea2dea27e60178d2bf0866a
nzambello <nicola@nzambello.dev>
2025-07-31 15:28:29 +0200

    feat: add white listed domains filter from layout config

--- src/index.tsx
+++ src/index.tsx
@@ -259,6 +259,22 @@ const Memori: React.FC<Props> = ({
   const layoutIntegrationConfig = safeParseJSON(
     layoutIntegration?.customData ?? '{}'
   );
+
+  const whiteListedDomains = layoutIntegrationConfig.whiteListedDomains;
+  if (whiteListedDomains) {
+    // check if we are client side
+    if (typeof window !== 'undefined') {
+      // check if the current domain is in the whiteListedDomains with Regex
+      if (
+        !whiteListedDomains.some((domain: string) =>
+          new RegExp(domain).test(window.location.hostname)
+        )
+      ) {
+        return null;
+      }
+    }
+  }
+
   const initialContextVars =
     context ?? getParsedContext(layoutIntegrationConfig.contextVars);
   const initialQuestionLayout =
