e48395c63dade40a29f5a25c82b8f817c74e396b
nzambello <nicola@nzambello.dev>
2025-08-29 12:16:08 +0200

    test: add snapshot test for index with all params

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -43,6 +43,56 @@ it('renders client', () => {
   expect(container).toMatchSnapshot();
 });
 
+it('renders client with all the props', () => {
+  const { container } = render(
+    <Memori
+      memoriID={memori.memoriID}
+      ownerUserID={memori.ownerUserID}
+      tenantID={tenant.tenantID}
+      integration={{
+        ...integration,
+        customData: JSON.stringify({}),
+      }}
+      secretToken="1234567890"
+      height="100%"
+      tag="🌻"
+      pin="123456"
+      showShare={true}
+      showChatHistory={true}
+      showCopyButton={true}
+      showTranslationOriginal={true}
+      showSettings={true}
+      showTypingText={true}
+      showClear={true}
+      showLogin={true}
+      showUpload={true}
+      showReasoning={true}
+      showContextPerLine={true}
+      context={{
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        phone: '+39 333 333 3333',
+      }}
+      initialQuestion="What is the capital of France?"
+      autoStart={true}
+      enableAudio={true}
+      defaultSpeakerActive={true}
+      useMathFormatting={true}
+      spokenLang="EL"
+      multilingual={true}
+      authToken="1234567890"
+      onStateChange={() => {}}
+      additionalInfo={{}}
+      customMediaRenderer={() => <div>Custom Media Renderer</div>}
+      additionalSettings={<div>Additional Settings</div>}
+      userAvatar="https://memori.ai/avatar.png"
+      applyVarsToRoot={true}
+      disableTextEnteredEvents={true}
+    />
+  );
+  expect(container).toMatchSnapshot();
+});
+
 it('renders client with whiteListedDomains on allowed domains', () => {
   mockLocation.hostname = 'memori.ai';
   const { container } = render(
