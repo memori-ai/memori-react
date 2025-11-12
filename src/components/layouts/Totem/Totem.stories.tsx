dcd098ec420d5bc54bd8b0fd5b0931948a6c5de4
andrepat0 <patini929@gmail.com>
2025-11-12 16:20:37 +0100

    refactor: update layout stories to use new Memori component structure and streamline props handling

--- src/components/layouts/Totem/Totem.stories.tsx
+++ src/components/layouts/Totem/Totem.stories.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { tenant } from '../../../mocks/data';
-import Memori, { Props } from '../../MemoriWidget/MemoriWidget';
+import { memori, tenant } from '../../../mocks/data';
+import { Props } from '../../MemoriWidget/MemoriWidget';
+import Memori from '../../../index';
 import I18nWrapper from '../../../I18nWrapper';
 import { VisemeProvider } from '../../../context/visemeContext';
 import { ArtifactProvider } from '../../MemoriArtifactSystem/context/ArtifactContext';
 
 const meta: Meta = {
   title: 'General/Layouts/Totem',
-  component: Memori,
-  argTypes: {
-    AZURE_COGNITIVE_SERVICES_TTS_KEY: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
+  component: (args: Props) => <Memori {...args} engineURL="https://engine.memori.ai" apiURL="https://backend.memori.ai" baseURL="https://www.aisuru.com"    />,
+  argTypes: {},
   parameters: {
     controls: { expanded: true },
     layout: 'fullscreen',
@@ -23,6 +18,7 @@ const meta: Meta = {
 };
 
 export default meta;
+
 
 const Template: Story<Props> = args => (
   <I18nWrapper>
@@ -34,51 +30,20 @@ const Template: Story<Props> = args => (
   </I18nWrapper>
 );
 
-const baseMemori = {
-  memoriID: '157860e8-f014-43b3-a7c5-fb4a08c87025',
-  name: 'Layout Storybook',
+const DefaultLayout = Template.bind({});
+DefaultLayout.args = {
+  memoriName: 'Layout Storybook',
   ownerUserName: 'andrea.patini',
+  memoriID: '157860e8-f014-43b3-a7c5-fb4a08c87025',
   ownerUserID: '95753bbe-9e88-4799-ae35-dc060bc11c48',
-  ownerTenantName: 'aisuru.com',
   tenantID: 'www.aisuru.com',
-  password: null,
-  recoveryTokens: null,
-  newPassword: null,
-  memoriConfigurationID: 'fd10bb42-98d9-4c08-8e02-2b08bd4e4975',
-  description: 'Layout Storybook Demo',
-  completionDescription: null,
-  engineMemoriID: '9b0a2913-d3d8-4e98-a49d-6e1c99479e1b',
-  isOwner: false,
-  isGiver: false,
-  isReceiver: false,
-  giverTag: null,
-  giverPIN: null,
-  privacyType: 'PUBLIC',
-  secretToken: null,
-  minimumNumberOfRecoveryTokens: null,
-  totalNumberOfRecoveryTokens: null,
-  sentInvitations: [],
-  receivedInvitations: [],
-  needsPosition: false,
-  voiceType: 'MALE',
-  culture: 'it-IT',
-  categories: ['demo', 'layout'],
-  exposed: true,
-  disableR2R3Loop: null,
-  disableR4Loop: null,
-  disableR5Loop: null,
-  enableCompletions: true,
-  completionModel: null,
-  chainingMemoriID: null,
-  chainingBaseURL: null,
-  chainingPassword: null,
-  contentQualityIndex: 100,
-  contentQualityIndexTimestamp: '2023-04-17T00:01:32.194744Z',
-  publishedInTheMetaverse: false,
-  metaverseEnvironment: null,
-  blockedUntil: null,
-  creationTimestamp: '2022-06-13T14:21:55.793034Z',
-  lastChangeTimestamp: '2023-04-15T08:15:36.403546Z',
+  engineURL: 'https://engine.memori.ai/memori/v2',
+  apiURL: 'https://backend.memori.ai/api/v2',
+  baseURL: 'https://www.aisuru.com',
+  layout: 'FULL_CHAT',
+  uiLang: 'IT',
+  spokenLang: 'IT',
+  integrationID: '0b1256c1-530c-4e67-aef8-36667c8887bb',
 };
 
 const baseIntegration = {
@@ -96,14 +61,9 @@ const baseIntegration = {
 
 export const TotemWithFullBodyAvatar = Template.bind({});
 TotemWithFullBodyAvatar.args = {
-  uiLang: 'it',
-  showShare: true,
-  showSettings: true,
-  memori: {
-    ...baseMemori,
+    ...DefaultLayout.args,
     avatar3DURL:
       'https://assets.memori.ai/api/v2/asset/692580d6-7b25-4ed0-84ce-82d5f4ac4270.glb#1762875775270',
-  },
   integration: {
     ...baseIntegration,
     customData: JSON.stringify({
@@ -126,14 +86,9 @@ TotemWithFullBodyAvatar.args = {
 
 export const TotemWithHalfBodyAvatar = Template.bind({});
 TotemWithHalfBodyAvatar.args = {
-  uiLang: 'it',
-  showShare: true,
-  showSettings: true,
-  memori: {
-    ...baseMemori,
+    ...DefaultLayout.args,
     avatar3DURL:
       'https://assets.memori.ai/api/v2/asset/acc38f4a-e4c3-4a21-9818-c3d1672820ea.glb#1762875973109',
-  },
   integration: {
     ...baseIntegration,
     customData: JSON.stringify({
@@ -156,10 +111,7 @@ TotemWithHalfBodyAvatar.args = {
 
 export const TotemWithDefaultTotem = Template.bind({});
 TotemWithDefaultTotem.args = {
-  uiLang: 'it',
-  showShare: true,
-  showSettings: true,
-  memori: baseMemori,
+  ...DefaultLayout.args,
   integration: {
     ...baseIntegration,
     customData: JSON.stringify({
--- src/components/layouts/layouts.stories.tsx
+++ src/components/layouts/layouts.stories.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { memori, tenant, integration } from '../../mocks/data';
-import Memori, { LayoutProps, Props } from '../MemoriWidget/MemoriWidget';
+import { LayoutProps, Props } from '../MemoriWidget/MemoriWidget';
+import Memori from '../../index';
 import I18nWrapper from '../../I18nWrapper';
 import Spin from '../ui/Spin';
 import { VisemeProvider } from '../../context/visemeContext';
 import { ArtifactProvider } from '../MemoriArtifactSystem/context/ArtifactContext';
 
+
 const meta: Meta = {
   title: 'General/Layouts',
-  component: Memori,
-  argTypes: {
-    AZURE_COGNITIVE_SERVICES_TTS_KEY: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
+  component: (args: Props) => <Memori {...args} engineURL="https://engine.memori.ai" apiURL="https://backend.memori.ai" baseURL="https://www.aisuru.com"    />,
+  argTypes: {},
   parameters: {
     controls: { expanded: true },
     layout: 'fullscreen',
@@ -25,105 +21,50 @@ const meta: Meta = {
 
 export default meta;
 
+
 const Template: Story<Props> = args => (
   <I18nWrapper>
-    <ArtifactProvider >
-    <VisemeProvider>
-      <Memori {...args} />
-    </VisemeProvider>
+    <ArtifactProvider>
+      <VisemeProvider>
+        <Memori {...args} />
+      </VisemeProvider>
     </ArtifactProvider>
   </I18nWrapper>
 );
-// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
-// https://storybook.js.org/docs/react/workflows/unit-testing
-const layoutStoryMemori = {
-  memoriID: '157860e8-f014-43b3-a7c5-fb4a08c87025',
-  name: 'Layout Storybook',
+
+const DefaultLayout = Template.bind({});
+DefaultLayout.args = {
+  memoriName: 'Layout Storybook',
   ownerUserName: 'andrea.patini',
+  memoriID: '157860e8-f014-43b3-a7c5-fb4a08c87025',
   ownerUserID: '95753bbe-9e88-4799-ae35-dc060bc11c48',
-  ownerTenantName: 'aisuru.com',
   tenantID: 'www.aisuru.com',
-  password: null,
-  recoveryTokens: null,
-  newPassword: null,
-  memoriConfigurationID: 'fd10bb42-98d9-4c08-8e02-2b08bd4e4975',
-  description: 'Layout Storybook Demo',
-  completionDescription: null,
-  engineMemoriID: '9b0a2913-d3d8-4e98-a49d-6e1c99479e1b',
-  isOwner: false,
-  isGiver: false,
-  isReceiver: false,
-  giverTag: null,
-  giverPIN: null,
-  privacyType: 'PUBLIC',
-  secretToken: null,
-  minimumNumberOfRecoveryTokens: null,
-  totalNumberOfRecoveryTokens: null,
-  sentInvitations: [],
-  receivedInvitations: [],
-  needsPosition: false,
-  voiceType: 'MALE',
-  culture: 'it-IT',
-  categories: ['demo', 'layout'],
-  exposed: true,
-  disableR2R3Loop: null,
-  disableR4Loop: null,
-  disableR5Loop: null,
-  enableCompletions: true,
-  completionModel: null,
-  chainingMemoriID: null,
-  chainingBaseURL: null,
-  chainingPassword: null,
-  contentQualityIndex: 100,
-  contentQualityIndexTimestamp: '2023-04-17T00:01:32.194744Z',
-  publishedInTheMetaverse: false,
-  metaverseEnvironment: null,
-  blockedUntil: null,
-  creationTimestamp: '2022-06-13T14:21:55.793034Z',
-  lastChangeTimestamp: '2023-04-15T08:15:36.403546Z',
+  engineURL: 'https://engine.memori.ai/memori/v2',
+  apiURL: 'https://backend.memori.ai/api/v2',
+  baseURL: 'https://www.aisuru.com',
+  layout: 'FULL_CHAT',
+  uiLang: 'IT',
+  spokenLang: 'IT',
+  integrationID: '0b1256c1-530c-4e67-aef8-36667c8887bb',
 };
 
-const layoutStoryIntegration = {
-  integrationID: '0b1256c1-530c-4e67-aef8-36667c8887bb',
-  memoriID: '157860e8-f014-43b3-a7c5-fb4a08c87025',
-  type: 'LANDING_EXPERIENCE',
-  state: 'NEW',
-  deviceEmails: null,
-  invocationText: null,
-  jobID: null,
-  publish: true,
-  creationTimestamp: '2022-06-13T14:44:52.833573Z',
-  lastChangeTimestamp: '2022-06-13T14:44:52.833573Z',
-};
 
 export const Default = Template.bind({});
 Default.args = {
-  uiLang: 'it',
-  showShare: true,
-  showSettings: true,
-  memori: layoutStoryMemori,
-  tenant,
-  layout: 'FULLPAGE',
+  ...DefaultLayout.args,
 };
 
-export const FullPage = Template.bind({});
-FullPage.args = {
-  uiLang: 'it',
-  showShare: true,
-  showSettings: true,
-  memori: layoutStoryMemori,
-  tenant,
-  layout: 'FULLPAGE',
-};
+
 
 export const Totem = Template.bind({});
 Totem.args = {
+  ...DefaultLayout.args,
   uiLang: 'it',
   showShare: true,
   showSettings: true,
-  memori: layoutStoryMemori,
+  memori: memori,
   integration: {
-    ...layoutStoryIntegration,
+    ...integration,
   },
   tenant,
   layout: 'TOTEM',
@@ -131,11 +72,12 @@ Totem.args = {
 
 export const ChatOnly = Template.bind({});
 ChatOnly.args = {
+  ...DefaultLayout.args,
   uiLang: 'it',
   showShare: true,
   showSettings: true,
   showUpload: true,
-  memori: layoutStoryMemori,
+  memori: memori,
   tenant,
   layout: 'CHAT',
 };
@@ -176,10 +118,11 @@ const CustomLayout: React.FC<LayoutProps> = ({
 
 export const Custom = Template.bind({});
 Custom.args = {
+  ...DefaultLayout.args,
   uiLang: 'it',
   showShare: true,
   showSettings: true,
-  memori: layoutStoryMemori,
+  memori: memori,
   tenant,
   layout: 'FULLPAGE',
   customLayout: CustomLayout,
@@ -187,33 +130,39 @@ Custom.args = {
 
 export const WebsiteAssistant = Template.bind({});
 WebsiteAssistant.args = {
+  ...DefaultLayout.args,
   uiLang: 'it',
   showShare: true,
   showSettings: true,
-  memori: layoutStoryMemori,
+  memori: memori,
   tenant,
   layout: 'WEBSITE_ASSISTANT',
 };
 
 export const HiddenChat = Template.bind({});
 HiddenChat.args = {
+  ...DefaultLayout.args,
   uiLang: 'it',
   showShare: true,
   showSettings: true,
-  memori: layoutStoryMemori,
+  memori: memori,
   tenant,
   layout: 'HIDDEN_CHAT',
 };
 
 export const ZoomedFullBody = Template.bind({});
 ZoomedFullBody.args = {
+  ...DefaultLayout.args,
   uiLang: 'it',
   showShare: true,
   showSettings: true,
   showAudio: true,
   enableAudio: true,
+  integration: {
+    
+  },
   memori: {
-    ...layoutStoryMemori,
+    ...memori,
     voiceType: 'FEMALE',
   },
   tenant,
