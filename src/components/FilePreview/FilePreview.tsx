b7f293503bae947fa4a0e9d1e3c00848509b1a85
andrepat0 <patini929@gmail.com>
2026-02-23 10:03:03 +0100

    fix: update file type display logic in FilePreview

--- src/components/FilePreview/FilePreview.tsx
+++ src/components/FilePreview/FilePreview.tsx
@@ -5,6 +5,7 @@ import Button from '../ui/Button';
 import ContentPreviewModal from '../ContentPreviewModal';
 import Snippet from '../Snippet/Snippet';
 import { stripHTML, stripDocumentAttachmentTags } from '../../helpers/utils';
+import { getFileExtensionFromMime } from '../MediaWidget/MediaItemWidget.utils';
 
 type FilePreviewProps = {
   previewFiles: any;
@@ -147,7 +148,9 @@ FilePreviewProps) => {
                 <div className="memori--preview-file-info">
                   <span className="memori--preview-filename">{file.name}</span>
                   <span className="memori--preview-filetype">
-                    {getFileType(file.name, file.type)}
+                    {file.mimeType
+                      ? getFileExtensionFromMime(file.mimeType)
+                      : getFileType(file.name, file.type)}
                   </span>
                 </div>
 
