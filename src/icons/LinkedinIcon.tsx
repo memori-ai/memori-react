92bed7807d0762c30615a571d56f36b958632a0e
andrepat0 <patini929@gmail.com>
2026-03-06 15:26:33 +0100

    feat: improve CSS styling for Blob component

--- src/icons/LinkedinIcon.tsx
+++ src/icons/LinkedinIcon.tsx
@@ -9,11 +9,11 @@ const LinkedinIcon = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill="#0F0F0F"
+      fill="currentColor"
       d="M6.5 8a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM11 19h1a1 1 0 0 0 1-1v-4.5c0-1.5 3-2.5 3-.5v5a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-6c0-2-1.5-3-3.5-3S13 10.5 13 10.5V10a1 1 0 0 0-1-1h-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1Z"
     />
     <path
-      fill="#0F0F0F"
+      fill="currentColor"
       fillRule="evenodd"
       d="M20 1a3 3 0 0 1 3 3v16a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3h16Zm0 2a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h16Z"
       clipRule="evenodd"
