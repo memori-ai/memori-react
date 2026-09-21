85e7860c6bdcfb70ced221ca4fec95c160ac1b86
andrepat0 <patini929@gmail.com>
2026-09-21 15:50:55 +0200

    feat(ContentPreviewModal): enhance modal functionality with scroll cues, markdown rendering, and improved layout

--- src/components/ContentPreviewModal/index.ts
+++ src/components/ContentPreviewModal/index.ts
@@ -1,2 +1,4 @@
 export { default } from './ContentPreviewModal';
 export type { ContentPreviewModalProps } from './ContentPreviewModal';
+export { default as DocumentMarkdown } from './DocumentMarkdown';
+export { prepareDocumentPreviewContent } from './DocumentMarkdown';
